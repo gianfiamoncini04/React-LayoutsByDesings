009e403efb18c5bd3897d3140eaee8dae5f70aeb
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2022-08-01 13:33:13 -0300

    Primeiro legítmo commit

---
+++ scaling-secrets/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+import Header from './components/Header/Header';
+// import Bodier from './components/Bodier/Bodier';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+}
---
+++ scaling-secrets/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+    return (
+        <>
+            <div className="header">
+                <div className="navbar">
+                    <img src="../img/logo.png" alt="logo" />
+                    <button>LOGIN</button>
+                </div>
+                <div className="text-intro">
+                    <h1>The Ultimate Guide To A Million Dollar eCommerce Store Even If <br /><span>You're A Beginner!</span></h1>
+                    <p>This program will transform you from someone that's just getting started in eCommerce to a power seller within 60 days or less!</p>
+                    <button>JOIN NOW</button>
+                </div>
+            </div>
+        </>
+    );
+}
