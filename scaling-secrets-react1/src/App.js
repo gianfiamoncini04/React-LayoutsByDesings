a9edb7e7d9f2f7117cf3c31f5ee98051ba20a79b
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2022-08-01 17:12:13 -0300

    SACO

--- scaling-secrets-react1/src/App.js
+++ scaling-secrets-react1/src/App.js
@@ -1,14 +1,15 @@
 import './styles/App.css';
 
 import Header from './components/Header/Header';
-// import Bodier from './components/Bodier/Bodier';
+import Bodier1 from './components/Bodier1/Bodier1';
+import Bodier2 from './components/Bodier2/Bodier2';
 
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <Header />
-      </header>
+      <Header />
+      <Bodier1 />
+      <Bodier2 />
     </div>
   );
 }
---
+++ scaling-secrets-react1/src/components/Bodier2/Bodier2.js
@@ -0,0 +1,20 @@
+export default function Bodier2() {
+    return (
+        <>
+            <div className="info">
+                <div className="info-text">
+                    <h1>Amaan Khan</h1>
+                    <h2>7 FIGURESS WALMART & FACEBOOK SHOPS ESELLER</h2>
+                    <p>I was once in the same boat as you, dreaming of being free from financial burdens. My mom always told me that if I worked hard enough, I could achieve anything. So that's what I did.<br /> <br /> <br />  When I got my degree, I thought it would be the ticket to a better life that I was dreaming of. Instead, like everyone else, I ended up in the 9-5 grind. The pay was good for a while. But after a few years, I noticed something.<br /> <br /> <br />  Every day I woke up exhausted, I started to hate the commute and felt stuck doing the same thing over and over again. To make matters worse, even though I graduated at the top 10% of my class, my work wasn't giving me any fulfillment.<br /> <br /> <br />  I realized that I needed to change. I wanted to be able to support my parents and myself without spending hours working at a desk -- to enjoy life and see what the rest of the world has to offer.</p>
+                </div>
+                <div className="img">
+                    <img src="/img/imagem-bodier2.png" alt="Exemplo" />
+
+                </div>
+                <div className="info-text-bot">
+                    <p>That's when I discovered eCommerce after coming across some videos of people selling stuff online. I loved how everything worked -- from sourcing products to building an online store. I was attracted to the freedom and having control of my time and my own growth.<br /> <br />  But It wasn't easy - in fact, it took years of trial and error before I finally created a blueprint for building a successful eCommerce store from scratch. And that's when things really started taking off - in total, my brother and I have made over 50 Million in sales.<br /> <br /> I'm glad I stuck with it, because now I'm doing what I love. I've built this business from zero into a multiple 8-figure empire, and now I want to teach you how to do the same.</p>
+                </div>
+            </div>
+        </>
+    );
+}
