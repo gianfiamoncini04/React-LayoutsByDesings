366bd30b47102f3b30ec4d42b05d82b5c139600d
gianfiamoncini04 <104286156+gianfiamoncini04@users.noreply.github.com>
2022-08-01 00:12:55 -0300

    kjhkhj

---
+++ fruit-salad-health-react2/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+import Header from './components/Header/Header';
+import Intro from './components/Intro/Intro';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
---
+++ fruit-salad-health-react2/src/components/Intro/Intro.js
@@ -0,0 +1,31 @@
+export default function Intro() {
+	return (
+		<>
+			<div className="intro">
+				<div className="intro-text">
+					<div className="topic">#1 Best Healthy Salad</div>
+					<div className="title"><span>Skip</span> The Diet, Just Eat Healthy With Jez Salad</div>
+					<div className="info">
+						Imagine you don't need a diet because we provide healthy and
+						delicious food for you!
+					</div>
+					<button>Order Food</button>
+					<div className="cur">
+						<div className="cur1">
+							5.000+ <br /> Customer
+						</div>
+						<div className="cur2">
+							8.000+ <br /> Delivery
+						</div>
+						<div className="cur3">
+							1.000+ <br /> Ratings
+						</div>
+					</div>
+				</div>
+                <div className="img">
+				    <img src="/img/back-intro.png" alt="client" />
+                </div>
+			</div>
+		</>
+	)
+}
